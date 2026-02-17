b7c9bbd37c75379b41d609347d5e32b97079b9df
RajeshGitAdmin <Rajesh.Gopu@lalsgroup.com>
2026-02-17 18:11:06 +0400

    Update SampleTable.sql
    Restore table to Previous Version

--- SQLGIT.SQLDatabase/dbo/Tables/SampleTable.sql
+++ SQLGIT.SQLDatabase/dbo/Tables/SampleTable.sql
@@ -1,11 +1,9 @@
 CREATE TABLE [dbo].[SampleTable] (
-    [SampleID]          INT            NOT NULL,
-    [SampleName]        NVARCHAR (255) NOT NULL,
-    [SampleDate]        DATE           NOT NULL,
-    [SampleDescription] NVARCHAR (500) NULL,
+    [SampleID]   INT            NOT NULL,
+    [SampleName] NVARCHAR (255) NOT NULL,
+    [SampleDate] DATE           NOT NULL,
     PRIMARY KEY CLUSTERED ([SampleID] ASC)
 );
 
 
 GO
-
