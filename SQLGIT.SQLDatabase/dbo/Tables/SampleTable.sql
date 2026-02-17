b988275d9c79eacb6e0ce2ff9edadeb7b59b565b
RajeshGitAdmin <Rajesh.Gopu@lalsgroup.com>
2026-02-17 17:41:37 +0400

    Committing 1 item from workspace 9209cbed-d76f-4709-80c0-a71647be6d57

--- SQLGIT.SQLDatabase/dbo/Tables/SampleTable.sql
+++ SQLGIT.SQLDatabase/dbo/Tables/SampleTable.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[SampleTable] (
-    [SampleID]   INT            NOT NULL,
-    [SampleName] NVARCHAR (255) NOT NULL,
-    [SampleDate] DATE           NOT NULL,
+    [SampleID]          INT            NOT NULL,
+    [SampleName]        NVARCHAR (255) NOT NULL,
+    [SampleDate]        DATE           NOT NULL,
+    [SampleDescription] NVARCHAR (500) NULL,
     PRIMARY KEY CLUSTERED ([SampleID] ASC)
 );
 
