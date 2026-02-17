60c4c2100f28c645b04ccd53bd0be55d0d7ddc14
RajeshGitAdmin <Rajesh.Gopu@lalsgroup.com>
2026-02-17 23:56:32 +0400

    Update SampleTable.sql

--- SQLGIT.SQLDatabase/dbo/Tables/SampleTable.sql
+++ SQLGIT.SQLDatabase/dbo/Tables/SampleTable.sql
@@ -2,9 +2,9 @@ CREATE TABLE [dbo].[SampleTable] (
     [SampleID]          INT            NOT NULL,
     [SampleName]        NVARCHAR (255) NOT NULL,
     [SampleDate]        DATE           NOT NULL,
-    [SampleDescription] NVARCHAR (500) NULL,
     PRIMARY KEY CLUSTERED ([SampleID] ASC)
 );
 
 
 GO
+
